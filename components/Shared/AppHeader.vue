547d5a419efe2a6ecc4fc2ceb14d81ac9a3e8292
Bedir Demir <bedir@proton.me>
2023-03-11 16:40:50 +0300

    Add: refresh button

--- components/Shared/AppHeader.vue
+++ components/Shared/AppHeader.vue
@@ -18,10 +18,19 @@
       </a>
     </div>
     <div>
-        <a class="flex items-center rounded text-xs lg:text-base bg-secondary text-white p-2 cursor-pointer active:opacity-75 transition-all" >
+        <a @click="getData" class="flex items-center rounded text-xs lg:text-base bg-secondary text-white p-2 cursor-pointer active:opacity-75 transition-all" >
             <svg class="w-4 lg:w-5 mr-1" fill="currentColor" version="1.1" viewBox="0 0 32 32"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M28,16c-1.219,0-1.797,0.859-2,1.766C25.269,21.03,22.167,26,16,26c-5.523,0-10-4.478-10-10S10.477,6,16,6  c2.24,0,4.295,0.753,5.96,2H20c-1.104,0-2,0.896-2,2s0.896,2,2,2h6c1.104,0,2-0.896,2-2V4c0-1.104-0.896-2-2-2s-2,0.896-2,2v0.518  C21.733,2.932,18.977,2,16,2C8.268,2,2,8.268,2,16s6.268,14,14,14c9.979,0,14-9.5,14-11.875C30,16.672,28.938,16,28,16z"/></svg>
             Yenile
         </a>
     </div>
   </header>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      getData: useEarthquakeStore().getData
+    }
+  }
+}
+</script>
