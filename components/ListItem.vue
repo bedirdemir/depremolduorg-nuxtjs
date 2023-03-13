c6bb34835a45f033a3b6347ce4bda5ab908121a6
Bedir Demir <bedir@proton.me>
2023-03-13 12:19:53 +0300

    Fix: locale time

--- components/ListItem.vue
+++ components/ListItem.vue
@@ -43,13 +43,13 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/tr';
 dayjs.extend(relativeTime);
-dayjs.locale('tr');
+// dayjs.locale('tr');
 
 export default {
   props: ['data'],
   methods: {
     getRelativeTime(date, time){
-      return dayjs(`${date} ${time}`, "YYYY.MM.DD hh:mm:ss").fromNow();
+      return dayjs(`${date} ${time}`, "YYYY.MM.DD hh:mm:ss").locale('tr').fromNow();
     },
     getDangerColor(magnitude){
       magnitude = Number(magnitude);
