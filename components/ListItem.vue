8f0f775f4151bf10b37d29fb82f4ded109cb064c
Bedir Demir <bedir@proton.me>
2023-03-16 04:13:24 +0300

    Update: earthquke list, header logo, navigation

--- components/ListItem.vue
+++ components/ListItem.vue
@@ -1,36 +1,34 @@
 <template>
   <li class="mb-2 shadow bg-[#FCFFE760]">
     <div class="flex justify-between p-3 border rounded w-full">
-      <div class="flex items-center gap-5 w-full">
-        <span :class="getDangerColor(data.magnitude)" class="flex flex-col items-center border rounded px-4 py-3 font-semibold text-lg lg:py-2 lg:px-3">
+      <div class="flex items-center gap-5 lg:gap-6 w-full">
+        <span :class="getDangerColor(data.magnitude)" class="flex flex-col justify-center items-center border rounded p-5 font-semibold text-xl lg:p-6 lg:text-2xl">
             {{ data.magnitude }}
           <span class=" text-xs font-normal">
             {{ data.scale }}
           </span>
         </span>
         <div class="flex flex-col w-full">
-          <div>
             <h2 class="text-sm lg:text-base font-semibold mb-1">{{ data.region }}</h2>
-              <div class="flex flex-col text-sm lg:text-base">
-                <div class="flex items-center">
-                  <svg class="w-4 inline-block text-gray-900" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960"><path d="M477 936q-149 0-253-105.5T120 575h60q0 125 86 213t211 88q127 0 215-89t88-216q0-124-89-209.5T477 276q-68 0-127.5 31T246 389h105v60H142V241h60v106q52-61 123.5-96T477 216q75 0 141 28t115.5 76.5Q783 369 811.5 434T840 574q0 75-28.5 141t-78 115Q684 879 618 907.5T477 936Zm128-197L451 587V373h60v189l137 134-43 43Z"/></svg>
-                  <span class="font-medium ml-1">{{ getRelativeTime(data.date, data.time) }}</span>
-                </div>
-                <div class="flex items-center">
-                  <svg class="w-4 inline-block text-gray-900" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960"><path d="M180 976q-24 0-42-18t-18-42V296q0-24 18-42t42-18h65v-60h65v60h340v-60h65v60h65q24 0 42 18t18 42v620q0 24-18 42t-42 18H180Zm0-60h600V486H180v430Zm0-490h600V296H180v130Zm0 0V296v130Zm300 230q-17 0-28.5-11.5T440 616q0-17 11.5-28.5T480 576q17 0 28.5 11.5T520 616q0 17-11.5 28.5T480 656Zm-160 0q-17 0-28.5-11.5T280 616q0-17 11.5-28.5T320 576q17 0 28.5 11.5T360 616q0 17-11.5 28.5T320 656Zm320 0q-17 0-28.5-11.5T600 616q0-17 11.5-28.5T640 576q17 0 28.5 11.5T680 616q0 17-11.5 28.5T640 656ZM480 816q-17 0-28.5-11.5T440 776q0-17 11.5-28.5T480 736q17 0 28.5 11.5T520 776q0 17-11.5 28.5T480 816Zm-160 0q-17 0-28.5-11.5T280 776q0-17 11.5-28.5T320 736q17 0 28.5 11.5T360 776q0 17-11.5 28.5T320 816Zm320 0q-17 0-28.5-11.5T600 776q0-17 11.5-28.5T640 736q17 0 28.5 11.5T680 776q0 17-11.5 28.5T640 816Z"/></svg>
-                  <time class="ml-1 text-stone-700">{{data.date}} - {{ data.time }}</time>
-                </div>
+            <div class="flex flex-col text-sm lg:text-base">
+              <div class="flex items-center">
+                <svg class="w-4 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
+                <span class="font-medium ml-1">{{ getRelativeTime(data.date, data.time) }}</span>
               </div>
-            </div>
-            <div class="flex justify-between items-center text-sm lg:text-base">
+              <div class="flex items-center">
+                <svg class="w-4 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                <time class="ml-1 text-stone-700">{{data.date}} - {{ data.time }}</time>
+              </div>
+                <div class="flex items-center">
+                <svg class="w-4 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><polyline points="19 12 12 19 5 12"></polyline></svg>
+                <span class="ml-1 text-stone-700">{{ data.depth }} km</span>
+              </div>
               <div class="flex items-center">     
-                <svg class="w-4 inline-block text-gray-900" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960"><path d="M480 976 200 696l42-42 208 208V176h60v686l208-208 42 42-280 280Z"/></svg>
-                  <span class="ml-1 text-stone-700">{{ data.depth }} km</span>
-              </div>
-                <a @click="earthquakeStore.modalToggle(data)"  class="underline text-sm cursor-pointer">
+                <svg class="w-4 text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
+                <a @click="earthquakeStore.modalToggle(data)" class="text-stone-700 underline cursor-pointer ml-1">
                     Konumu görüntüle
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="w-3 inline-block"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
                 </a>
+              </div>
             </div>
         </div>
       </div>
--- components/Shared/AppHeader.vue
+++ components/Shared/AppHeader.vue
@@ -1,20 +1,9 @@
 <template>
   <header class="flex justify-between items-center pt-3 pb-2 lg:pt-4 px-2 lg:px-0 bg-[#FCFFE760] lg:bg-inherit">
     <div class="flex items-center space-x-1">
-      <svg class="w-10 lg:w-12 text-primary" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 47 47" xml:space="preserve">
-        <g>
-          <g>
-            <path d="M24.104,41.577c-0.025,0-0.053-0.001-0.078-0.001c-1.093-0.035-2.025-0.802-2.271-1.867l-5.46-23.659l-3.199,8.316
-                    c-0.357,0.93-1.252,1.544-2.249,1.544H2.41c-1.331,0-2.41-1.079-2.41-2.41c0-1.331,1.079-2.41,2.41-2.41h6.78l5.433-14.122
-                    c0.38-0.989,1.351-1.612,2.418-1.54c1.057,0.074,1.941,0.831,2.18,1.863l5.186,22.474l4.618-15.394
-                    c0.276-0.923,1.078-1.592,2.035-1.702c0.953-0.107,1.889,0.36,2.365,1.198l4.127,7.222h7.037c1.331,0,2.41,1.079,2.41,2.41
-                    c0,1.331-1.079,2.41-2.41,2.41h-8.436c-0.865,0-1.666-0.463-2.094-1.214l-2.033-3.559l-5.616,18.722
-                    C26.104,40.88,25.164,41.577,24.104,41.577z"/>
-          </g>
-        </g>
-      </svg>
+      <svg class="w-8 lg:w-12 text-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline></svg>
       <a href="https://depremoldu.org/">
-        <h1 class="text-secondary text-lg lg:text-xl">depremoldu.org</h1>
+        <h1 class="text-secondary text-base font-medium lg:text-xl">depremoldu.org</h1>
       </a>
     </div>
     <div>
