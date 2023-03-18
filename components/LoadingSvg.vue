5eee1cf71a94ba6f835b7c9f18d022af3b73557f
Bedir Demir <bedir@proton.me>
2023-03-18 21:47:57 +0300

    Update: loading animation

--- components/LoadingSvg.vue
+++ components/LoadingSvg.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex min-h-[75vh] text-primary justify-center items-center">
-    <div class="font-light">Yükleniyor...</div>
+    <div class="spinner"></div>
   </div>
 </template>
