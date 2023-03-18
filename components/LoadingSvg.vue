4045011ce351e713479d28768383b627453e562a
Bedir Demir <bedir@proton.me>
2023-03-18 21:50:27 +0300

    Fix: loading-h

--- components/LoadingSvg.vue
+++ components/LoadingSvg.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-[75vh] text-primary justify-center items-center">
+  <div class="flex min-h-[70vh] text-primary justify-center items-center">
     <div class="spinner"></div>
   </div>
 </template>
